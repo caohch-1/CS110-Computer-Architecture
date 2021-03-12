8b4656a35d06ed937810ba68b38874b69e720ba4
MissSirius <57584450+caohch-1@users.noreply.github.com>
2021-03-12 21:38:02 +0800

    Delete HW2 directory

--- HW2/dynamiclib.sh
+++
@@ -1,3 +0,0 @@
-gcc -c -Wpedantic -Wall -Wextra -Werror -std=c89 -fpic doubll2d.c
-gcc -Wpedantic -Wall -Wextra -Werror -std=c89 -shared -o liblist.so doubll2d.o
-gcc -Wpedantic -Wall -Wextra -Werror -std=c89 -L. -o dynamiclist test.c -l:liblist.so
