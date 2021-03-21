32e88f48a63177201f987d9336514fdd54e4419c
caohch-1 <2267408610@qq.com>
2021-03-21 21:40:41 +0800

    Lab4.0.2

--- Lab4/first1pos.s
+++ Lab4/first1pos.s
@@ -67,9 +67,41 @@ main:
 
 first1posshift:
 	### YOUR CODE HERE ###
+    li t1 32 # t1 = 32
+    loop:
+    beqz t1 end # if t1 == 0: end
+    srli t2 a1 31 # t2 = test_num >> 31
+    slli a1 a1 1 # test_num = test_num << 1
+    addi t1 t1 -1 # t1 -= 1
+    beqz t2 loop # if t2 == 0: loop
+    
+    mv a0 t1 # a0 = t1
+    ret
+    
+    end:	
+    li a0 -1 # a0 = -1
+    ret
+    
+    
 
 first1posmask:
 	### YOUR CODE HERE ###
+    li t1 32 # t1 = 32
+    li t2 0x80000000 #mask = 0x80000000
+    loop2:
+    beqz t1 end2 # if t1 == 0: end
+    and t3 a1 t2 # t3= test_num & mask
+    addi t1 t1 -1 # t1 -= 1
+    srl t3 t3 t1 # t3 = t3 >> t1
+    srli t2 t2 1 # mask = mask >> 1
+    beqz t3 loop2 # if t3 == 0: loop2
+    
+    mv a0 t1 # a0 = t1
+    ret
+    
+    end2:
+    li a0 -1 # a0 = -1
+    ret
 
 print_int:
 	mv	a1, a0
--- Lab4/test.c
+++ Lab4/test.c
@@ -2,11 +2,11 @@
 
 int main() {
     int i = 0;
-    int test_num = 0x0010;
-    while ((test_num>>31) != 0x1) {
+    int test_num = 0x00010000;
+    // printf("%0x", test_num<<31);
+    for(i=31; i>= 0; i--) {
+        if ((test_num >> 31) != 0) break;
         test_num = test_num<<1;
-        printf("%0x\t", (test_num>>31));
-        i ++;
     }
 
     printf("%d\n", i);
