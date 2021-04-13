517c6e098ad4c1cd23d08a5c9c421cfc3f72c008
caohch-1 <2267408610@qq.com>
2021-04-13 14:30:33 +0800

    Lab6

--- Lab5/test.sh
+++ Lab5/test.sh
@@ -3,7 +3,7 @@
 # Just run this file and you can test your circ files!
 # Make sure your files are in the directory above this one though!
 
-cp ex1.circ ex2.circ ex3.circ testing/circ_files
+cp ex1.circ ex2.circ ex3.circ ex4.circ ex5.circ testing/circ_files
 
 cd testing
 rm -rf student_output
